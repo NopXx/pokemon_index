92504b64f638a0412ab85b09fd6ec4625a7c641d
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-12 21:08:10 +0700

    update edit style

--- js/index.js
+++ js/index.js
@@ -1,24 +1,40 @@
 const pokemon = document.getElementById('pokemon');
 
+const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+const formath = (str) => {
+    console.log(str);
+    if (str.toString().length === 1) {
+        return '000' + str
+    } else if (str.toString().length === 2) {
+        return '00' + str
+    } else {
+        return '0' + str
+    }
+}
 
 fetch("../pokemon_data.json")
-.then(response => {
-   return response.json();
-}).then((response) => {
-    const data = response
-    data.forEach((result) => {
-        pokemon.innerHTML += `<div class="col-md-4">
+    .then(response => {
+        return response.json();
+    }).then((response) => {
+        const data = response
+        data.forEach((result) => {
+            pokemon.innerHTML += `
+        <a href="detail/${result.id}"
+        <div class="col-md-4">
         <div class="card p-3 mb-2 shadow-sm">
           <div class="d-flex justify-content-between">
               <div class="d-flex flex-row align-items-center">
                   <div class="ms-2 c-details">
-                      <h4 class="mb-0" id="name">${result.name}</h4> <span id="id"><h6>${result.id}</h6></span>
+                      <h4 class="mb-0" id="name">${capitalizeFirstLetter(result.name)}</h4> <span>${formath(result.id)}</span>
                   </div>
               </div>
               <div class="badge" id="types"> 
               ${result.types.map((type) => (
-                `<span class="${type.name}">${type.name}</span>`
-    ))}
+                `<span class="${type.name}">${capitalizeFirstLetter(type.name)}</span>`
+                ))}
                 
               </div>
           </div>
@@ -28,8 +44,8 @@ fetch("../pokemon_data.json")
             </div>
           </div>
       </div>
-      </div>`
+      </div>
+      </a>
+      `
+        })
     })
-})
-
-
