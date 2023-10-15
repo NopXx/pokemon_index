ec80dd283073b0e63daaede69150ea550ca4171d
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-15 13:16:39 +0700

    add get & show pokemon by id

--- js/detail.js
+++ js/detail.js
@@ -2,11 +2,45 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const modal_pokemon = document.getElementById('modal-pokemon');
 const modal_input = document.getElementById('modal-input');
+const style_image = localStorage.getItem('style-image');
+const save = document.getElementById('save-image')
+const stats = document.getElementById('stats')
+const character = document.getElementById('character');
+
+if (style_image != null) {
+    console.log(style_image)
+    if (style_image === '1') {
+        document.getElementById('image-default').checked = true;
+    } else {
+        document.getElementById('image-pix').checked = true;
+    }
+} else {
+    localStorage.setItem('style-image', 1)
+    document.getElementById('image-default').checked = true;
+}
+
+var image_show = style_image == '1' ? 'image' : 'image_pix'
+
+const setWidthBar = (str, stat) => {
+    if (str === 'hp') {
+        return (stat / 255) * 100
+    } else if (str === "attack") {
+        return (stat / 190) * 100
+    } else if (str === "defense") {
+        return (stat / 250) * 100
+    } else if (str === "special-attack") {
+        return (stat / 150) * 100
+    } else if (str === "special-defense") {
+        return (stat / 250) * 100
+    } else {
+        return (stat / 200) * 100
+    }
+}
 
 const id = urlParams.get('id')
 console.log(id);
 
-const data = fetch('./pokemon_data_v2.json')
+const data = fetch('./pokemon_data.json')
     .then(response => response.json())
     .then(data => {
         return data;
@@ -18,6 +52,70 @@ const data = fetch('./pokemon_data_v2.json')
 const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+const showDetail = () => {
+    data.then((data) => {
+        const list = data.filter(function (pokemon) {
+            return pokemon.id.toString() == id;
+        });
+        console.log(list);
+        const color = list[0].types[0].name
+        document.getElementById("back-color").classList.add(color)
+        list[0].stats.forEach((item) => {
+            stats.innerHTML += `
+            <div class="row">
+                <div class="col-3 text-end">
+                    <p>${capitalizeFirstLetter(item.name)}</p>
+                </div>
+                <div class="col-8 ">
+                    <div class="progress mt-1" style="height: 20px;">
+                        <div class="progress-bar ${color}" role="progressbar" style="width: ${setWidthBar(item.name,item.base_stat)}%;">${item.base_stat}</div>
+                    </div>
+                </div>
+            </div>`
+        })
+        list.forEach((item) => {
+            character.innerHTML = `
+            <h1>${capitalizeFirstLetter(item.name)}</h2>
+            <p>#${item.id}</p>
+            <div class="text-center my-3">
+                <img src="${item[image_show]}" class=" rounded-circle p-4" alt="${item.name}" style="background-color: whitesmoke; width: 70%;">
+            </div>
+            `
+            document.getElementById('detail').innerHTML = `
+            <div class="row">
+                <div class="col-4 text-end">
+                <p class="fs-5">Height : </p>
+                </div>
+                <div class="col-auto text-start">
+                <p class="fs-5">${item.height / 10}</p>
+                </div>
+                <div class="col-auto text-start">
+                <p class="fs-5">m </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 text-end">
+                <p class="fs-5">Weight : </p>
+                </div>
+                <div class="col-auto text-start">
+                <p class="fs-5">${item.weight / 10}</p>
+                </div>
+                <div class="col-auto text-start">
+                <p class="fs-5">kg</p>
+                </div>
+            </div>
+            `
+        })
+        const type = document.getElementById('types')
+        list[0].types.forEach((item) => {
+            type.innerHTML += `<span class="type ${item.name} mx-1 px-3 py-1 Typedropdown">${capitalizeFirstLetter(item.name)}</span>`
+        })
+    })
+}
+
+showDetail()
+
 
 modal_input.addEventListener('input', (e) => {
     const value = e.target.value.toLowerCase()
@@ -36,7 +134,7 @@ modal_input.addEventListener('input', (e) => {
                         <div
                     class="d-flex justify-content-between align-items-center modal-card" style="cursor: pointer;">
                     <div class="d-flex flex-row align-items-center">
-                      <img src="${result.image}" class="modal-image" alt="">
+                      <img src="${result[image_show]}" class="modal-image" alt="">
                       <div class="d-flex flex-column">
                         <span>${result.name}</span>
                         <div
@@ -58,4 +156,17 @@ modal_input.addEventListener('input', (e) => {
             })
         }, modal_pokemon);
     }
+})
+
+save.addEventListener('click', () => {
+    const radio = document.querySelectorAll('input[name="toggle')
+    let selectedSize;
+    for (const radioButton of radio) {
+        if (radioButton.checked) {
+            selectedSize = radioButton.value;
+            break;
+        }
+    }
+    localStorage.setItem('style-image', selectedSize)
+    location.reload();
 })
