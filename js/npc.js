cdfccb2a82cb169565fad6c5f14d2ea3c028048a
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-31 15:41:25 +0700

    Update npc.js

--- js/npc.js
+++ js/npc.js
@@ -1,8 +1,8 @@
 export const npc = [{
     "id": 1,
     "name": "Red",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/6/66/Spr_RG_Red_1.png",
-    "image": "https://archives.bulbagarden.net/media/upload/d/d3/Lets_Go_Pikachu_Eevee_Red.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698739801/image/pokemon/npc/Red_pix_z4ujzv.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698739925/image/pokemon/npc/Red_lf1xhw.png",
     "age": 18,
     "gender": "Male",
     "trainer": [{
@@ -22,8 +22,8 @@ export const npc = [{
   {
     "id": 2,
     "name": "Blue Oak",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/9/92/Spr_RG_Blue_1.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/1/1a/Lets_Go_Pikachu_Eevee_Blue.png/320px-Lets_Go_Pikachu_Eevee_Blue.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740893/image/pokemon/npc/Blue_Oak_pix_h4by3n.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740893/image/pokemon/npc/Blue_Oak_m6h0xf.png",
     "age": 18,
     "gender": "Male",
     "trainer": [{
@@ -51,8 +51,8 @@ export const npc = [{
   {
     "id": 3,
     "name": "Agatha",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/f/f4/Spr_RG_Agatha.png",
-    "image": "https://img.wattpad.com/ee67b974f3f3c921ac876b9c86b4dc9874fb3638/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f76446a7a33466a4a5368334832773d3d2d3930333839373237362e313631383836383738303835613965363932323037393334323330362e706e67?s=fit&w=720&h=720",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740889/image/pokemon/npc/Agatha_pix_cpfybl.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740890/image/pokemon/npc/Agatha_vo3cah.png",
     "age": "Unknow",
     "gender": "Female",
     "trainer": [{
@@ -68,8 +68,8 @@ export const npc = [{
   {
     "id": 4,
     "name": "Koichi",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/4/48/Spr_RG_Blackbelt.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/e/ee/Karate_Master_Journey.png/250px-Karate_Master_Journey.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740910/image/pokemon/npc/Koichi_pix_rinujw.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740909/image/pokemon/npc/Koichi_xrwc4w.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -85,8 +85,8 @@ export const npc = [{
   {
     "id": 5,
     "name": "Lance",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/e/eb/Spr_RG_Lance.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/c/cd/Lets_Go_Pikachu_Eevee_Lance.png/220px-Lets_Go_Pikachu_Eevee_Lance.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740911/image/pokemon/npc/Lance_pix_wnlbce.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740910/image/pokemon/npc/Lance_vvf8gf.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -106,8 +106,8 @@ export const npc = [{
   {
     "id": 6,
     "name": "Lorelei",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/5/58/Spr_RG_Lorelei.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/f/f7/Lets_Go_Pikachu_Eevee_Lorelei.png/162px-Lets_Go_Pikachu_Eevee_Lorelei.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740915/image/pokemon/npc/Lorelei_pix_tvyie5.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740915/image/pokemon/npc/Lorelei_gpagha.png",
     "age": "Unknow",
     "gender": "Female",
     "trainer": [{
@@ -118,8 +118,8 @@ export const npc = [{
   {
     "id": 7,
     "name": "Lt. Surge",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/a/a2/Spr_RG_Lt_Surge.png",
-    "image": "https://archives.bulbagarden.net/media/upload/archive/b/bc/20180809132420%21Lets_Go_Pikachu_Eevee_Lt_Surge.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740919/image/pokemon/npc/Lt_Surge_pix_cjpjmc.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740916/image/pokemon/npc/Lt_Surge_y8e8fx.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -135,8 +135,8 @@ export const npc = [{
   {
     "id": 8,
     "name": "Bill",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/7/71/TCG1_A40_Bill.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/0/0b/FireRed_LeafGreen_Bill.png/125px-FireRed_LeafGreen_Bill.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740889/image/pokemon/npc/Bill_pix_igfgnb.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740889/image/pokemon/npc/Bill_dnc8jl.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -147,8 +147,8 @@ export const npc = [{
   {
     "id": 9,
     "name": "Blaine",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/6/6e/Spr_RG_Blaine.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/c/c8/Lets_Go_Pikachu_Eevee_Blaine.png/216px-Lets_Go_Pikachu_Eevee_Blaine.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740892/image/pokemon/npc/Blaine_pix_h9vrwh.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740890/image/pokemon/npc/Blaine_ihhcjv.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -164,8 +164,8 @@ export const npc = [{
   {
     "id": 10,
     "name": "Brock",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/e/e2/Spr_RG_Brock.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/a/a6/Lets_Go_Pikachu_Eevee_Brock.png/121px-Lets_Go_Pikachu_Eevee_Brock.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740895/image/pokemon/npc/Brock_pix_v2o2be.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740894/image/pokemon/npc/Brock_ngonux.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -189,8 +189,8 @@ export const npc = [{
   {
     "id": 11,
     "name": "Bruno",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/7/76/Spr_RG_Bruno.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/4/4c/Lets_Go_Pikachu_Eevee_Bruno.png/200px-Lets_Go_Pikachu_Eevee_Bruno.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740897/image/pokemon/npc/Bruno_pix_zntthr.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740898/image/pokemon/npc/Erika_ijwali.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -201,8 +201,8 @@ export const npc = [{
   {
     "id": 12,
     "name": "Erika",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/e/ee/Spr_RG_Erika.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/4/47/Lets_Go_Pikachu_Eevee_Erika.png/120px-Lets_Go_Pikachu_Eevee_Erika.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740898/image/pokemon/npc/Erika_pix_ghop3n.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740898/image/pokemon/npc/Erika_ijwali.png",
     "age": "Unknow",
     "gender": "Female",
     "trainer": [{
@@ -218,8 +218,8 @@ export const npc = [{
   {
     "id": 13,
     "name": "Giovanni",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/2/23/Spr_RG_Giovanni.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/a/a7/Lets_Go_Pikachu_Eevee_Giovanni.png/220px-Lets_Go_Pikachu_Eevee_Giovanni.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740901/image/pokemon/npc/Giovanni_pix_noxway.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740899/image/pokemon/npc/Giovanni_iymnqq.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -255,8 +255,8 @@ export const npc = [{
   {
     "id": 14,
     "name": "James",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/thumb/a/a0/VSJames_PE.png/90px-VSJames_PE.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/1/19/James_JN.png/159px-James_JN.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740902/image/pokemon/npc/James_pix_a1b05a.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740902/image/pokemon/npc/James_pmkplh.png",
     "age": 25,
     "gender": "Male",
     "trainer": [{
@@ -284,8 +284,8 @@ export const npc = [{
   {
     "id": 15,
     "name": "Jessie",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/thumb/1/12/VSJessie_PE.png/100px-VSJessie_PE.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/0/0d/Jessie_JN.png/187px-Jessie_JN.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740905/image/pokemon/npc/Jessie_pix_pmmzll.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740904/image/pokemon/npc/Jessie_skgqdf.png",
     "age": 26,
     "gender": "Female",
     "trainer": [{
@@ -321,8 +321,8 @@ export const npc = [{
   {
     "id": 16,
     "name": "Koga",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/b/b0/Spr_RG_Koga.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/f/f4/Lets_Go_Pikachu_Eevee_Koga.png/225px-Lets_Go_Pikachu_Eevee_Koga.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740907/image/pokemon/npc/Koga_pix_fva5yt.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740906/image/pokemon/npc/Koga_mbbapo.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -338,8 +338,8 @@ export const npc = [{
   {
     "id": 17,
     "name": "Misty",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/2/2d/Spr_RG_Misty.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/f/f6/Lets_Go_Pikachu_Eevee_Misty.png/183px-Lets_Go_Pikachu_Eevee_Misty.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740920/image/pokemon/npc/Misty_pix_j63tfg.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740919/image/pokemon/npc/Misty_vqtbul.png",
     "age": "Unknow",
     "gender": "Female",
     "trainer": [{
@@ -355,8 +355,8 @@ export const npc = [{
   {
     "id": 18,
     "name": "Old man",
-    "image": "https://archives.bulbagarden.net/media/upload/a/a4/FRLG_Old_man_Back.png",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/3/30/RB_Old_man_Back.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740924/image/pokemon/npc/Old_man_dmxcdx.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740925/image/pokemon/npc/Old_man_pix_yzg84k.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
@@ -367,8 +367,8 @@ export const npc = [{
   {
     "id": 19,
     "name": "Professor Samuel Oak",
-    "image_pix": "https://archives.bulbagarden.net/media/upload/1/1e/Spr_RG_Oak.png",
-    "image": "https://archives.bulbagarden.net/media/upload/thumb/3/3e/Lets_Go_Pikachu_Eevee_Professor_Oak.png/180px-Lets_Go_Pikachu_Eevee_Professor_Oak.png",
+    "image_pix": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740924/image/pokemon/npc/Oak_pix_rdtt5k.png",
+    "image": "https://res.cloudinary.com/dqolakmsp/image/upload/v1698740889/image/pokemon/npc/Oak_ca59ft.png",
     "age": "Unknow",
     "gender": "Male",
     "trainer": [{
